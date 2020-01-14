c10f562593fd7a3a929772f03fc2c41b15226de0
andreventuravale <andreventuravale@gmail.com>
2020-01-14 13:14:46 -0300

    'all' offsetter

--- test/offsetter.test.js
+++ test/offsetter.test.js
@@ -1,8 +1,16 @@
 const { expect } = require('chai')
 
+const all = require('../src/offsetter/all')
 const skipper = require('../src/offsetter/skipper')
 
 suite('offsetters', () => {
+  suite('all', () => {
+    test('pass along the offset without any modification', () => {
+      expect(all('', 0)).to.deep.eql(0)
+      expect(all('', 10)).to.deep.eql(10)
+    })
+  })
+
   suite('skipper', () => {
     test('skip whitespace - gives a new offset', () => {
       const offset = skipper(/\s+/y)('  1', 0)
