f4971db6f7c5a12aa51b80b17d8578d4a6023fac
andreventuravale <andreventuravale@gmail.com>
2020-01-14 14:21:35 -0300

    Refactoring

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const all = require('./offsetter/all')
 const choose = require('./choose')
 const compose = require('./compose')
 const hidden = require('./decorator/hidden')
@@ -5,13 +6,16 @@ const list = require('./list')
 const optional = require('./optional')
 const skipper = require('./offsetter/skipper')
 const token = require('./token')
+const wsSkip = require('./offsetter/wsSkip')
 
 module.exports = {
+  all,
   choose,
   compose,
   hidden,
   list,
   optional,
   skipper,
-  token
+  token,
+  wsSkip
 }
