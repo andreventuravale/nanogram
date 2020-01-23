0b6f3ef456a386253c5958cc040b6d5b2d81cfd3
andreventuravale <andreventuravale@gmail.com>
2020-01-22 21:05:49 -0300

    x

--- test/v3.test.js
+++ test/v3.test.js
@@ -33,8 +33,23 @@ const grammar = function (build) {
   return result
 }
 
+const factor = grammar => {
+  const hash = {}
+  const r = []
+  for (const [key, value] of grammar) {
+    if (!hash[key]) {
+      hash[key] = []
+      r.push([key, hash[key]])
+    }
+    hash[key].push(value)
+  }
+  return r.map(([key, value]) => {
+    return [key, value.length === 1 ? value[0] : value]
+  })
+}
+
 suite('v3', () => {
-  test('', () => {
+  test('grammar', () => {
     const result = grammar(({ define, choose, optional, repeat, concat }) => {
       define('asterisk', /\*/)
       define('digit', /\d/)
@@ -70,6 +85,51 @@ suite('v3', () => {
       ['factor', [['paropen', 'expr', 'parclose'], ',']],
       ['term', 'factor'],
       ['term', [['term', 'asterisk', 'factor'], ',']]
+    ])
+  })
+
+  test('factor', () => {
+    const result = factor(grammar(({ define, choose, optional, repeat, concat }) => {
+      define('asterisk', /\*/)
+      define('digit', /\d/)
+      define('id', /\w/)
+      define('minus', /-/)
+      define('parclose', /\)/)
+      define('paropen', /\(/)
+      define('plus', /\+/)
+
+      define('expr', concat('expr', 'minus', 'term'))
+      define('expr', concat('term'))
+
+      define('factor', choose('id', 'digit'))
+      define('factor', concat('paropen', 'expr', 'parclose'))
+
+      define('term', 'factor')
+      define('term', concat('term', 'asterisk', 'factor'))
+    }))
+
+    require('clipboardy').writeSync(JSON.stringify(result, 0, 2))
+
+    expect(result).to.eql([
+      ['asterisk', /\*/],
+      ['digit', /\d/],
+      ['id', /\w/],
+      ['minus', /-/],
+      ['parclose', /\)/],
+      ['paropen', /\(/],
+      ['plus', /\+/],
+      ['expr', [
+        [['expr', 'minus', 'term'], ','],
+        [['term'], ',']
+      ]],
+      ['factor', [
+        [['id', 'digit'], '|'],
+        [['paropen', 'expr', 'parclose'], ',']
+      ]],
+      ['term', [
+        'factor',
+        [['term', 'asterisk', 'factor'], ',']
+      ]]
     ])
   })
 })
